6d2e92eba70152209433782ff22a43aa95319a1e
Thisisus7 <rick840905528@gmail.com>
2023-08-22 22:55:04 +0800

    add basename for BrowserRouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';  // for useNavigate
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/QA-Editor">
       <App />
     </BrowserRouter>
   </React.StrictMode>
